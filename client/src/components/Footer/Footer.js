5dfe86894c4a7b7c9182061a607ccb3cc7d9d2a9
Tuscany <lizlevine.rigie@gmail.com>
2020-10-10 16:24:05 -0400

    feat: Add assets dir with social imgs

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
 import "./style.css";
 
+function Footer() {
+  return (
+    <div>
+      <footer className="text-muted">
+        <div className="footer">
+          <p className="thanks">
+            <a href="#">You can find me here:</a>
+            <p className="copyright">&copy; copyright 2020 elizabeth levine</p>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
 
-
-
-export default Footer; 
+export default Footer;
