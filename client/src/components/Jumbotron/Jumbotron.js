c27c006bf2dcfd1f80e1208e8c887f7b6b83aad1
Tuscany <lizlevine.rigie@gmail.com>
2020-10-14 20:44:10 -0400

    feat: Add jumbotron component

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./style.css";
 
 
-
+function Jumbotron() {
+    return (
+      <div className="jumbotron jumbotron-fluid">
+        <div className="jumbo">
+          <h1 className="display-4">John Ryan</h1>
+          <p className="lead">
+            The clicker method of training is a tried, true and fun method of
+            teaching your dog good social behavior. Click on any dog to begin.{" "}
+            <br></br>
+            (But each dog only gets one CLICK | REWARD per game!)
+          </p>
+        </div>
+      </div>
+    );
+  }
 
 
 export default Jumbotron; 
