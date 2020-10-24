d13ec93959bc17f7e44940006433d04e11d485ba
Tuscany <lizlevine.rigie@gmail.com>
2020-10-24 19:18:20 -0400

    feat: Add container as placeholder for further content

---
+++ client/src/components/Container/Container.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Container() {
+  return (
+      <div>
+          <container  className="main">
+
+          </container>
+      </div>
+
+);
+}
+
+export default Container;
