f3f40d71b87fa59907243766efdec66ecd1d1646
Tuscany <lizlevine.rigie@gmail.com>
2020-10-26 08:46:26 -0400

    chore: Importcontainer to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from "./components/NavBar/NavBar";
 import Jumbotron from "./components/Jumbotron/Jumbotron";
 import Footer from "./components/Footer/Footer";
+import Container from "./components/Container/Container";
 
 class App extends React.Component {
 render() {
