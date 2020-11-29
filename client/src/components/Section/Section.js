4daee3e0135cc5ae46b886d46a443fb3bf575cce
Tuscany <lizlevine.rigie@gmail.com>
2020-11-29 18:32:27 -0500

    feat: Addsection.js

--- client/src/components/Section/Section.js
+++ client/src/components/Section/Section.js
@@ -16,8 +16,9 @@ Tart sugar plum croissant icing tiramisu sesame snaps ice cream liquorice powder
 
 Bear claw jelly beans toffee jelly-o chocolate bar pastry. Topping cookie carrot cake soufflé apple pie dragée cupcake wafer. Donut lollipop jelly beans soufflé. Fruitcake soufflé jujubes macaroon ice cream.
       </p>
+      <p>
       Carrot cake candy canes tootsie roll jujubes gingerbread soufflé sweet. Halvah chocolate cake gummies dessert marzipan cheesecake liquorice. Dessert toffee bear claw chocolate chocolate cake pastry. Biscuit fruitcake tiramisu fruitcake caramels donut icing. Liquorice biscuit cake cupcake. Tootsie roll pudding carrot cake apple pie jelly beans candy gummi bears. Sugar plum macaroon croissant cake cheesecake. Liquorice caramels chocolate cake carrot cake. Fruitcake danish soufflé carrot cake. Dragée biscuit toffee lemon drops topping brownie candy canes. Sweet roll biscuit caramels wafer bonbon macaroon wafer. Halvah candy liquorice danish. Sweet brownie carrot cake jujubes. Cake gingerbread halvah dragée candy canes tart liquorice pie.
-   
+      </p>
     </section>
   );
 }
